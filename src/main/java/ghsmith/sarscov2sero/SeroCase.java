23c84b65ccb6d693df3f86ffedf9fb32fc4d8bd0
ghsmith <geoffrey.hughes.smith@gmail.com>
2020-04-07 08:57:25 -0400

    Release candidate.

---
+++ src/main/java/ghsmith/sarscov2sero/SeroCase.java
@@ -0,0 +1,14 @@
+package ghsmith.sarscov2sero;
+
+/**
+ *
+ * @author Geoffrey H. Smith, MD
+ */
+public class SeroCase {
+
+    public String caseId;
+    public int daysAfterPcr;
+    public String standardResult;
+    public float ourResult;
+    
+}
---
+++ src/main/java/ghsmith/sarscov2sero/SeroCaseFinder.java
@@ -0,0 +1,41 @@
+package ghsmith.sarscov2sero;
+
+import java.io.File;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.util.ArrayList;
+import java.util.List;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVParser;
+import org.apache.commons.csv.CSVRecord;
+
+/**
+ *
+ * @author Geoffrey H. Smith, MD
+ */
+public class SeroCaseFinder {
+
+    File file;
+    List<SeroCase> seroCases;
+
+    public SeroCaseFinder(File file) {
+        this.file = file;
+    }
+    
+    public List<SeroCase> getAll() throws IOException {
+        if(seroCases == null) {
+            seroCases = new ArrayList<>();
+            CSVParser csvParser = CSVParser.parse(file, Charset.defaultCharset(), CSVFormat.DEFAULT.withFirstRecordAsHeader());
+            for(CSVRecord csvRecord : csvParser) {
+                SeroCase seroCase = new SeroCase();
+                seroCases.add(seroCase);
+                seroCase.caseId = csvRecord.get("caseId");
+                seroCase.daysAfterPcr = csvRecord.get("daysAfterPcr") != null && csvRecord.get("daysAfterPcr").length() > 0 ? Integer.valueOf(csvRecord.get("daysAfterPcr")) : 0;
+                seroCase.standardResult = csvRecord.get("standardResult");
+                seroCase.ourResult = Float.valueOf(csvRecord.get("ourResult"));
+            }
+        }
+        return seroCases;
+    }
+    
+}
